7703c9052f37dfc8b5db7112d633e65e25de0842
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-08-02 16:45:51 +0700

    update hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,43 @@
+import data from '../data/hero.json'
+
 export const Hero = () => {
+    const {
+        firstName, lastName, profession, avatar, socialMedia, contacts,
+    } = data;
+
     return (
         <section>
-            <div class="container">
-                <h1>Hero Section</h1>
+            <div class="container grid-2">
+                <div class="avatar p-5">
+                    <img src={avatar.src} alt="Avatar" width="200" height="200" />
+                </div>
+                <div class="p-5 mt-auto content">
+                    <h1 class="name">
+                        <span class="first-name">{firstName}</span>
+                        <br/>
+                        <span class="last-name">{lastName}</span>
+                    </h1>
+                    <div class="d-flex justify-between align-center flex-wrap">
+                        <p class="mr-3">{profession}</p>
+                        <div class="d-flex gap-x-1">
+                            {socialMedia.map((i) => (
+                                    <a href={i.url} target="_blank" key={i.name} >
+                                        <img src={i.src} width="30" height="30" class="rounded" />
+                                    </a>
+                                ))}
+                        </div>
+                    </div>
+                    <div class="contacts d-flex justify-between align-center flex-wrap gap-x-3">
+                        {contacts.map((i) => (
+                            <div key={i.name}>
+                                <p class="text-bold">{i.name}</p>
+                                <p>
+                                    <a href={i.url}>{i.content}</a>
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </section>
     )
