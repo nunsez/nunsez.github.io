a60cc3c9d24eea75e0b0f0a4543f2bfa3699307e
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-08-03 13:52:06 +0700

    update About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,18 @@
+import data from '../data/about.json';
+
 export const About = () => {
+    const { header, description, content } = data;
+
     return (
         <section>
-            <div class="container">
-                <h1>About Section</h1>
+            <div class="container grid-2">
+                <div class="p-5 text-end">
+                    <p class="text-bold">{header.toUpperCase()}</p>
+                    <p>{description}</p>
+                </div>
+                <div class="p-5">
+                    {content.map((i) => <p key={i}>{i}</p>)}
+                </div>
             </div>
         </section>
     )
