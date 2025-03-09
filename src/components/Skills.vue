9ce516fb75714dd8024e433f3a6a2855ec0e6160
Alexander Mandrikov <mandrikov@pm.me>
2025-03-09 16:42:29 +0700

    update paths for skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -7,25 +7,17 @@
       <div class="p-5 skills-grid">
         <Skill name="Ruby" ico="/skills/ruby.png" href="https://rubyonrails.org/" />
         <Skill name="Ruby on Rails" ico="/skills/rubyonrails.png" href="https://rubyonrails.org/" />
-        <Skill name="Elixir" ico="/public/skills/elixir.png" href="https://elixir-lang.org/" />
+        <Skill name="Elixir" ico="/skills/elixir.png" href="https://elixir-lang.org/" />
         <Skill
           name="Phoenix"
-          ico="/public/skills/phoenixframework.png"
+          ico="/skills/phoenixframework.png"
           href="https://www.phoenixframework.org/"
         />
-        <Skill
-          name="TypeScript"
-          ico="/public/skills/ts.png"
-          href="https://www.typescriptlang.org"
-        />
-        <Skill name="NodeJS" ico="/public/skills/node.png" href="https://nodejs.dev/" />
-        <Skill
-          name="PostgreSQL"
-          ico="/public/skills/postgresql.png"
-          href="https://www.postgresql.org/"
-        />
-        <Skill name="Docker" ico="/public/skills/docker.png" href="https://www.docker.com/" />
-        <Skill name="Git" ico="/public/skills/git.png" href="https://git-scm.com/" />
+        <Skill name="TypeScript" ico="/skills/ts.png" href="https://www.typescriptlang.org" />
+        <Skill name="NodeJS" ico="/skills/node.png" href="https://nodejs.dev/" />
+        <Skill name="PostgreSQL" ico="/skills/postgresql.png" href="https://www.postgresql.org/" />
+        <Skill name="Docker" ico="/skills/docker.png" href="https://www.docker.com/" />
+        <Skill name="Git" ico="/skills/git.png" href="https://git-scm.com/" />
       </div>
     </div>
   </section>
